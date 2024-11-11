eb85e52b18e34bcd5d1c7fe11f4d7c4107bc378d
feelindeadinside <nfomin66@gmail.com>
2024-11-11 23:08:29 +0300

    Add design 2 windows

--- AutomatedInformationSystem/obj/Debug/net8.0-windows/App.g.cs
+++ AutomatedInformationSystem/obj/Debug/net8.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace AutomatedInformationSystem {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace AutomatedInformationSystem {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public static void Main() {
             AutomatedInformationSystem.App app = new AutomatedInformationSystem.App();
             app.InitializeComponent();
